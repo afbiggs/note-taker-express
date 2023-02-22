899b535091267dff99e319efe98bab59650c7e09
afbiggs <112245251+afbiggs@users.noreply.github.com>
2023-02-21 20:15:42 -0500

    changes in server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const app = express();
-
-
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
+
+const PORT = process.env.PORT || 3001;
 
 // parse POST data
 app.use(express.urlencoded({ extended: true }));
@@ -11,11 +11,11 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-app.use('/api', apiRoutes);
-app.use('/', htmlRoutes);
+app.get('/api', apiRoutes);
+app.get('/', htmlRoutes);
 
 app.use(express.static('public'));
 
-app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-});
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
