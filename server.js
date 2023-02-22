41f71d2d808cb463ef76f68d9ac4d49418c5314b
afbiggs <112245251+afbiggs@users.noreply.github.com>
2023-02-21 20:23:12 -0500

    added route for homepage in server.js

--- server.js
+++ server.js
@@ -11,10 +11,15 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-app.get('/api', apiRoutes);
-app.get('/', htmlRoutes);
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
 
 app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
